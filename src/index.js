674157db6ea39eb72e394ee2ecc1103e40a73bc4
DrewFrost <drew.vfrost@gmail.com>
2018-12-26 22:25:09 +0200

    added functionality

--- src/index.js
+++ src/index.js
@@ -1,27 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './seasondisplay';
+import Loader from './loader.js'
 
 class App extends React.Component{
-    constructor(props){
-        super(props);
-
-        this.state = {lat: null };
+    state = {lat: null, errorMessage:""};
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude})
-            },
-            err => alert("Position wasn't given")
+            position => this.setState({ lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})   
         );
-
     }
-
-
-    render(){
-        
-        return(<div>Latitude: {this.state.lat}</div>);
+    renderContent(){
+        if(this.state.errorMessage && !this.state.lat){
+            return(
+                <div>{this.state.errorMessage}</div>
+            );
+        }
+        if(!this.state.errorMessage && this.state.lat){
+            return(
+                <SeasonDisplay lat={this.state.lat}/>
+            );
+        }
+        return(
+            <Loader message="Please accept location request"/>
+        )
 
     };
+    render(){
+       return(
+           <div >
+           {this.renderContent()}
+           </div>
+       )
+        
+        };    
+        };
 
-};
+
+
+
 ReactDOM.render(<App/>, document.querySelector('#root'));
